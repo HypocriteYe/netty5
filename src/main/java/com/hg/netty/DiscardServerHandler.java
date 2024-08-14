2394624082f95a20acd0bb8865db9c3f67af17cd
ygl <934752370@qq.com>
2024-08-14 09:51:08 +0800

    commit

---
+++ src/main/java/com/hg/netty/DiscardServerHandler.java
@@ -0,0 +1,24 @@
+package com.hg.netty;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @description 处理服务端通道
+ * @Author ygl
+ * @Create 2024/8/12 18:53
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
---
+++ src/main/java/com/hg/netty/bio/BIOServer.java
@@ -0,0 +1,9 @@
+package com.hg.netty.bio;
+
+/**
+ * @description
+ * @Author ygl
+ * @Create 2024/8/14 09:46
+ */
+public class BIOServer {
+}
